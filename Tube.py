56bbd190de1fade1c12aacbe5423b03bb4ba42db
Kolyasik <81467032+Kolyasik-Dungen@users.noreply.github.com>
2022-01-31 21:02:24 +0600

    Revert "Correct collision"

--- Tube.py
+++ Tube.py
@@ -22,7 +22,6 @@ class Tube(pygame.sprite.Sprite):
         super().__init__(*group)
         self.y = randint(-285, -75)
         self.image = Tube.image
-        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = 1050
         self.rect.y = self.y
